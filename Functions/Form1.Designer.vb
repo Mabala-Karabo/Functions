c29794c3652ca9a8995b01e3a265bf4acecc9991
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-18 17:00:33 +0200

    Add project files.

---
+++ Functions/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        txtNum1 = New TextBox()
+        txtNum2 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI Semibold", 14F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button1.Location = New Point(317, 160)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(218, 88)
+        Button1.TabIndex = 0
+        Button1.Text = "Execute functions"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' txtNum1
+        ' 
+        txtNum1.Location = New Point(79, 2)
+        txtNum1.Name = "txtNum1"
+        txtNum1.Size = New Size(150, 31)
+        txtNum1.TabIndex = 1
+        ' 
+        ' txtNum2
+        ' 
+        txtNum2.Location = New Point(420, 14)
+        txtNum2.Name = "txtNum2"
+        txtNum2.Size = New Size(150, 31)
+        txtNum2.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtNum2)
+        Controls.Add(txtNum1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+End Class
---
+++ Functions/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim result As Integer
+        result = addFunction(txtNum1.Text, txtNum2.Text)
+        MsgBox("The sum is " & result)
+    End Sub
+
+    Private Function addFunction(ByVal a As Integer, ByVal b As Integer) As Integer
+        Dim sum As Integer
+        sum = a + b
+        Return sum
+    End Function
+End Class
